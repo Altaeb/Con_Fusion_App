e345530b70e96251df524966fadde7fb37bf3338
Altaeb <abd2010ly@gmail.com>
2020-06-17 14:56:32 +0200

    first commit

---
+++ public/precache-manifest.a53de92e71d9a67012b45ef3667888a5.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c58b87a60ebf3c55129112b5211ddc0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a8110ed4a1c2c3f6808b",
+    "url": "/static/css/2.13eaaff5.chunk.css"
+  },
+  {
+    "revision": "1a412888f49e8f2d2add",
+    "url": "/static/css/main.9db786e3.chunk.css"
+  },
+  {
+    "revision": "a8110ed4a1c2c3f6808b",
+    "url": "/static/js/2.adb2f260.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.adb2f260.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a412888f49e8f2d2add",
+    "url": "/static/js/main.f3187f5c.chunk.js"
+  },
+  {
+    "revision": "84926f692196015996db",
+    "url": "/static/js/runtime-main.c71f3b90.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
